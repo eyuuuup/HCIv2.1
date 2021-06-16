e847e6490c0649c09f944f3fae1e87ea60348dcf
eyupams <43216769+eyupams@users.noreply.github.com>
2021-06-16 18:27:34 +0200

    added cancel button and changed css typo

--- contact/index.js
+++ contact/index.js
@@ -2,4 +2,13 @@ var sumbit_click = function(){
     window.location.href = "../redirect.html";
 }; 
 
+
+var reset_click = function () {
+    document.getElementById("inputname").value = "";
+    document.getElementById("inputsubject").value = "";
+    document.getElementById("inputmail").value = "";
+    document.getElementById("inputdesc").value = "";
+ }
+
 document.getElementById('sumbit').onclick = sumbit_click;
+document.getElementById('cancel').onclick = reset_click;
