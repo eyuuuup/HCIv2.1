95c49aeb9bb377f477f9f32aa6987f5673a68dde
rutgerkool <rutgerkool@gmail.com>
2021-06-21 15:45:51 +0200

    Moved files to root folder

---
+++ language-set.js
@@ -0,0 +1,7 @@
+function changeLanguage(languageId){
+    if(languageId==1){
+        document.getElementById('dropdown07').innerHTML ='<img src="../language.png" style="height: 1.5em"> EN';
+    } else if(languageId==2){
+        document.getElementById('dropdown07').innerHTML ='<img src="../language.png" style="height: 1.5em"> ES';
+    }
+  }
